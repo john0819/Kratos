966218af1e9999480f60c6878df37619de7d844c
john.wong <188490555@qq.com>
2025-06-01 18:02:57 +0800

    feat::增加可选鉴权接口白名单

--- internal/pkg/middleware/auth/auth.go
+++ internal/pkg/middleware/auth/auth.go
@@ -2,10 +2,10 @@ package auth
 
 import (
 	"context"
-	"errors"
 	"strings"
 	"time"
 
+	"github.com/go-kratos/kratos/v2/errors"
 	"github.com/go-kratos/kratos/v2/middleware"
 	"github.com/go-kratos/kratos/v2/transport"
 	"github.com/golang-jwt/jwt/v4"
@@ -14,6 +14,11 @@ import (
 const (
 	tokenWord = "Token"
 )
+
+// 可选鉴权接口
+var optionalAuthRouters = map[string]struct{}{
+	"/realworld.v1.RealWorld/GetProfile": {},
+}
 
 // 在context里面存储用户信息-uid
 // learn: 专门用一个字段来存储用户信息
@@ -43,20 +48,28 @@ func GenerateToken(secret string, userid uint) string {
 
 // server层的middleware 做auth鉴权 - path前缀来判断是否需要鉴权
 // path前缀 - 白名单 - server层做的
+// 可选鉴权接口 需要在这层做判断 - 有token则判断， 没有则跳过
 func JWTAuth(secret string) middleware.Middleware {
 	return func(handler middleware.Handler) middleware.Handler {
 		return func(ctx context.Context, req interface{}) (reply interface{}, err error) {
 			if tr, ok := transport.FromServerContext(ctx); ok {
+				// 获取当前接口路径
+				currentPath := tr.Operation()
+				// 获取token
 				tokenString := tr.RequestHeader().Get("Authorization")
 
 				if tokenString == "" {
-					return nil, errors.New("token is required")
+					// 可选鉴权接口 - 没有token则跳过
+					if _, ok := optionalAuthRouters[currentPath]; ok {
+						return handler(ctx, req)
+					}
+					return nil, errors.Unauthorized("UNAUTHORIZED", "token is required")
 				}
 
 				// 切割字符串 token的格式组成
 				auths := strings.SplitN(tokenString, " ", 2)
 				if len(auths) != 2 || !strings.EqualFold(auths[0], tokenWord) {
-					return nil, errors.New("invalid token")
+					return nil, errors.Unauthorized("UNAUTHORIZED", "invalid token")
 				}
 				tokenString = auths[1]
 
@@ -65,7 +78,7 @@ func JWTAuth(secret string) middleware.Middleware {
 					return []byte(secret), nil
 				}, jwt.WithValidMethods([]string{jwt.SigningMethodHS256.Alg()}))
 				if err != nil {
-					return nil, err
+					return nil, errors.Unauthorized("UNAUTHORIZED", "invalid token")
 				}
 
 				// jwt中的payload部分 - 数据
@@ -77,7 +90,7 @@ func JWTAuth(secret string) middleware.Middleware {
 						ctx = WithContext(ctx, &CurrentUser{UserID: uint(u.(float64))})
 					}
 				} else {
-					return nil, errors.New("invalid token")
+					return nil, errors.Unauthorized("UNAUTHORIZED", "invalid token claims")
 				}
 			}
 			return handler(ctx, req)
